188aa019dcad90115494b9f90977e4d9c771f295
sfxoawoyomi <sfxoawoyomi@semfix.com>
2022-03-28 17:51:24 +0100

    added create ingredient feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -27,7 +27,8 @@ class TagViewSet(viewsets.GenericViewSet,
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manage Ingredient in the database"""
     authentication_classes = (TokenAuthentication,)
     # permission is given once authenticated
@@ -40,6 +41,6 @@ class IngredientViewSet(viewsets.GenericViewSet,
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    # def perform_create(self, serializer):
-    #     """Create a new tag"""
-    #     serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
