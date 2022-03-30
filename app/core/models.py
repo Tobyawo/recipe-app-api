e85a0a28da5123d4b1fa57edeb284f2521dce4c3
sfxoawoyomi <sfxoawoyomi@semfix.com>
2022-03-30 11:35:19 +0100

    added image upload and filtering by tags and ingredients feature

--- app/core/models.py
+++ app/core/models.py
@@ -6,12 +6,14 @@ from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.conf import settings
 
+
 def recipe_image_file_path(instance, filename):
     """generate file path for new recipe image"""
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
     return os.path.join('uploads/recipe/', filename)
+
 
 class UserManager(BaseUserManager):
 
